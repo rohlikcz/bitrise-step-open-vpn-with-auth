6a26d90fe01cf86f74d67e0d681ca7248185cf2f
Frederico Gonçalves <fgoncalves@babbel.com>
2019-10-07 12:48:43 +0200

    Remove all logs

--- step.sh
+++ step.sh
@@ -32,47 +32,16 @@ EOF
     echo ${user} > /etc/openvpn/auth.txt
     echo ${password} >> /etc/openvpn/auth.txt
 
-    echo -e 'OMIT_SENDSIGS=0\nOPTARGS="--syslog"' > /etc/default/openvpn
-    echo "/etc/default/openvpn"
-    cat /etc/default/openvpn
-    echo ""
-
-    ls /var/log
-
     # We start the VPN service. By default, openvpn takes the client.conf file from the path /etc/openvpn
     sudo systemctl enable openvpn@client.service
     sudo service openvpn start
 
-    sleep 5
+    sleep 10
     
     sudo cat /var/log/dmesg
 
     # We add the DNS IP addresses and search domain to resolve the domains correctly
     echo -e "nameserver ${vpn_dns} ${vpn_dns2}\nsearch ${search_domain}\n$(cat /etc/resolv.conf)" > /etc/resolv.conf
-    
-    echo "/etc/openvpn/client.conf"
-    cat /etc/openvpn/client.conf
-    echo ""
-    
-    echo "/etc/openvpn/ca.crt"
-    cat  /etc/openvpn/ca.crt
-    echo ""
-    
-    echo "/etc/openvpn/ta.key"
-    cat /etc/openvpn/ta.key
-    echo ""
-    
-    echo "/etc/openvpn/auth.txt"
-    cat /etc/openvpn/auth.txt
-    echo ""
-    
-    echo "/etc/openvpn/auth.txt"
-    cat /etc/openvpn/auth.txt
-    echo ""
-    
-    echo "/etc/resolv.conf"
-    cat /etc/resolv.conf
-    echo ""
     
     ifconfig
     
