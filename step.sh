cdc3fca4750abfa6bdd0efe858319842adeac004
Martin Egermajer <egermajer.martin@gmail.com>
2020-11-20 11:10:43 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -37,8 +37,9 @@ EOF
 ${ovpn_file}
 EOF
     # Write the certificate, key and credentials to respective files
-    echo ${user} > ./auth.txt
-    echo ${password} >> ./auth.txt
+    sudo touch /etc/openvpn/auth.txt
+    sudo echo ${user} > /etc/openvpn/auth.txt
+    sudo echo ${password} >> /etc/openvpn/auth.txt
 
     # We call openvpn as a command, indicating all the necessary parameters by command line
     #sudo openvpn --client --tls-client --remote-cert-tls server --resolv-retry infinite --dev tun --proto ${proto} --remote ${host} ${port} --auth-user-pass auth.txt --auth SHA256 --persist-key --persist-tun --compress lz4-v2 --cipher AES-256-CBC --ca ca.crt --tls-auth ta.key --key-direction 1 > /dev/null 2>&1 &
@@ -53,30 +54,30 @@ EOF
     #sudo sed -i '' "2i10.20.0.18  git.rhldev.cz" /etc/hosts
 
     # Traverse the macOS network adapters and set the DNS IP addresses and search domain for each one
-    IFS=$'\n'
+    #IFS=$'\n'
      
     # VPN DNS Server IP addresses and search domain
-    vpndns="10.20.0.5"
-    vpndns2="10.20.0.1"
+    #vpndns="10.20.0.5"
+    #vpndns2="10.20.0.1"
     #searchdomain=${search_domain}
     
-    adapters=`networksetup -listallnetworkservices |grep -v denotes`
+    #adapters=`networksetup -listallnetworkservices |grep -v denotes`
      
      
-    for adapter in $adapters
-    do
-            echo updating dns for $adapter
-            dnssvr=(`networksetup -getdnsservers $adapter`)
-     
-            if [ $dnssvr != $vpndns ]; then
-                    # We set the DNS IP addresses of the VPN
-                    networksetup -setdnsservers $adapter $vpndns $vpndns2
-                    #networksetup -setsearchdomains $adapter $searchdomain
-                    else
-                    # We reverse the DNS IP address to the originals
-                    networksetup -setdnsservers $adapter empty
-            fi
-    done
+    #for adapter in $adapters
+    #do
+    #        echo updating dns for $adapter
+    #        dnssvr=(`networksetup -getdnsservers $adapter`)
+    # 
+    #        if [ $dnssvr != $vpndns ]; then
+    #                # We set the DNS IP addresses of the VPN
+    #                networksetup -setdnsservers $adapter $vpndns $vpndns2
+    #                #networksetup -setsearchdomains $adapter $searchdomain
+    #                else
+    #                # We reverse the DNS IP address to the originals
+    #                networksetup -setdnsservers $adapter empty
+    #        fi
+    #done
 
 ifconfig
 
