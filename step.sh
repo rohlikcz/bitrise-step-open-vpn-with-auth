49ba0153e3f4fdbdc963a3ac8630b403e8c65fdd
Martin Egermajer <egermajer.martin@gmail.com>
2020-11-20 13:11:17 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -31,7 +31,6 @@ EOF
     echo "Configuring for Mac OS"
     
     mkdir ./openvpn
-    #touch ./openvpn/client.conf
     
     # We create the .conf file with the parameters of the VPN, including the authorization through the txt file
     cat <<EOF > ./openvpn/client.conf
@@ -42,55 +41,11 @@ EOF
     echo ${user} > ./openvpn/auth.txt
     echo ${password} >> ./openvpn/auth.txt
     
-    echo "---client---"
-    cat ./openvpn/client.conf
+    #For debug add '--log ./openvpn/ovpn-pls.log --verb 5' and call 'sudo cat ./openvpn/ovpn-pls.log'
+    sudo openvpn --config ./openvpn/client.conf > /dev/null 2>&1 &
     
-    echo "---auth---"
-    cat ./openvpn/auth.txt
-
-    # We call openvpn as a command, indicating all the necessary parameters by command line
-    #sudo openvpn --client --tls-client --remote-cert-tls server --resolv-retry infinite --dev tun --proto ${proto} --remote ${host} ${port} --auth-user-pass auth.txt --auth SHA256 --persist-key --persist-tun --compress lz4-v2 --cipher AES-256-CBC --ca ca.crt --tls-auth ta.key --key-direction 1 > /dev/null 2>&1 &
-    #sudo openvpn --config ./client.conf > /dev/null 2>&1 &
-    sudo openvpn --config ./openvpn/client.conf --log ./openvpn/ovpn-pls.log --verb 5 &
     
     sleep 5
-    
-    echo "yay"
-    
-    sudo cat ./openvpn/ovpn-pls.log
-    
-    #cat /etc/hosts
-    
-    #sudo echo "10.20.0.18 git.rhldev.cz" >> /etc/hosts
-    #sudo sed -i '' "2i10.20.0.18  git.rhldev.cz" /etc/hosts
-
-    # Traverse the macOS network adapters and set the DNS IP addresses and search domain for each one
-    #IFS=$'\n'
-     
-    # VPN DNS Server IP addresses and search domain
-    #vpndns="10.20.0.5"
-    #vpndns2="10.20.0.1"
-    #searchdomain=${search_domain}
-    
-    #adapters=`networksetup -listallnetworkservices |grep -v denotes`
-     
-     
-    #for adapter in $adapters
-    #do
-    #        echo updating dns for $adapter
-    #        dnssvr=(`networksetup -getdnsservers $adapter`)
-    # 
-    #        if [ $dnssvr != $vpndns ]; then
-    #                # We set the DNS IP addresses of the VPN
-    #                networksetup -setdnsservers $adapter $vpndns $vpndns2
-    #                #networksetup -setsearchdomains $adapter $searchdomain
-    #                else
-    #                # We reverse the DNS IP address to the originals
-    #                networksetup -setdnsservers $adapter empty
-    #        fi
-    #done
-
-ifconfig
 
     if ifconfig -l | grep utun0 > /dev/null
     then
