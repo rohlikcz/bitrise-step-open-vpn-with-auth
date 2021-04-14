efb525d516947f382b94cf7affc19199336ab309
Martin Egermajer <egermajer.martin@gmail.com>
2021-04-14 13:01:20 +0200

    Update step.sh

--- step.sh
+++ step.sh
@@ -33,9 +33,9 @@ EOF
     #service openvpn start
     openvpn --config /etc/openvpn/client.conf --auth-user-pass /etc/openvpn/auth.conf &
     
-    echo "sleeping 15s"
-    sleep 15
-    echo "Oh, what a nice sleep, so much rest"
+    echo "$now Sleeping"
+    sleep 60
+    echo "$now Fully awake"
     
     if ifconfig | grep tun0 > /dev/null
     then
@@ -62,9 +62,9 @@ EOF
     #For debug add '--log ./openvpn/ovpn-pls.log --verb 5' and call 'sudo cat ./openvpn/ovpn-pls.log'
     sudo openvpn --config ./openvpn/client.conf --auth-user-pass ./openvpn/auth.conf > /dev/null 2>&1 &
     
-    echo "sleeping 15s"
-    sleep 15
-    echo "Oh, what a nice sleep, so much rest"
+    echo "$(date) Sleeping"
+    sleep 60
+    echo "$(date) Fully awake"
 
     if ifconfig -l | grep utun0 > /dev/null
     then
